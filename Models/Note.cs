61c33f0db59711c53db2db333dc891cfd85eeed1
João Schmidt <jvsc.dev@outlook.com>
2020-10-26 02:48:50 -0300

    Adicionar arquivos de projeto.

---
+++ Models/Note.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Notes_API.Models
+{
+    public class Note
+    {
+        public long Id { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public string CreatedAt { get; set; }
+        public string DeadLine { get; set; }
+        public bool IsComplete { get; set; }
+    }
+}
